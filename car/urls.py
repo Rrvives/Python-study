f7597f920a66a6798a72827f07012060a19fb2af
gzx <xiaoguo4072@163.com>
2025-06-12 14:01:31 +0800

    fix(gzx):学习

--- car/urls.py
+++ car/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from . import views
+from .views import AddVehicleView, VehicleListView, UpdateVehiclePricesView, DeleteVehiclesView
 
 urlpatterns = [
-    path('add_vehicle/', views.add_vehicle, name='add_vehicle'),
-    path('vehicles/', views.VehicleListView.as_view(), name='vehicle-list'),
-    path('update_prices/', views.update_vehicle_prices, name='update_prices'),
-    path('delete_vehicles/', views.delete_vehicles, name='delete_vehicles'),
+    path('vehicles/add/', AddVehicleView.as_view(), name='add-vehicle'),
+    path('vehicles/', VehicleListView.as_view(), name='vehicle-list'),
+    path('vehicles/update-prices/', UpdateVehiclePricesView.as_view(), name='update-vehicle-prices'),
+    path('vehicles/delete-old/', DeleteVehiclesView.as_view(), name='delete-vehicles'),
 ]
