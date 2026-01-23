19240aeb3cf42835f919945b72951361a44274d8
郭志雄 <guozhixiong@guoyusec.com>
2026-01-23 14:05:26 +0800

    项目搭建

--- 02-文件操作/07-json.py
+++
@@ -1,8 +0,0 @@
-import json
-
-data = [{"a": "测试"}]
-json_str = json.dumps(data, ensure_ascii=False)
-print(json_str)
-data1 = '[{"a": "测试"}]'
-l = json.loads(data1)
-print(l)
--- 06-spark/01-spark.py
+++
@@ -1,4 +0,0 @@
-from pyspark import SparkConf, SparkContext
-
-conf = SparkConf().setMaster("local").setAppName("MyApp")
-SparkContext.getOrCreate(conf=conf)
